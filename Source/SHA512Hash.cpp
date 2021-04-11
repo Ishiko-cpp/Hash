dfc0d58a91e6268201343738b1092ba5bdd46e75
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-11 02:11:12 +0100

    Replace OpenSSL with Botan

--- Source/SHA512Hash.cpp
+++ Source/SHA512Hash.cpp
@@ -1,23 +1,7 @@
 /*
-    Copyright (c) 2017 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2017-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Hash/blob/main/LICENSE.txt
 */
 
 #include "SHA512Hash.h"
@@ -30,12 +14,11 @@ namespace Hash
 
 SHA512Hash::SHA512Hash()
 {
-    int err = SHA512_Init(&m_context);
 }
 
 void SHA512Hash::update(const char* data, size_t length)
 {
-    int err = SHA512_Update(&m_context, (unsigned char*)data, length);
+    m_context.update((const uint8_t*)data, length);
 }
 
 void SHA512Hash::updateFromFile(const std::string& filePath)
@@ -56,7 +39,7 @@ void SHA512Hash::updateFromFile(const std::string& filePath)
 
 const std::array<unsigned char, 64>& SHA512Hash::value() const
 {
-    int err = SHA512_Final(m_value.data(), &m_context);
+    m_context.final(m_value.data());
     return m_value;
 }
 
