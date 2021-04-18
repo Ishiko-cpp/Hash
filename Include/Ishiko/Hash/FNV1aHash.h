1689309079ffca677bf1926405c37cd4b31d2236
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-18 12:21:09 +0100

    Implement and test FNV1a hash (#11)

---
+++ Include/Ishiko/Hash/FNV1aHash.h
@@ -0,0 +1,37 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Hash/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_HASH_FNV1AHASH_H_
+#define _ISHIKO_HASH_FNV1AHASH_H_
+
+#include <cstddef>
+#include <cstdint>
+
+namespace Ishiko
+{
+namespace Hash
+{
+
+// TODO: for now we only support 64 bit hash   
+class FNV1aHash
+{
+public:
+    FNV1aHash();
+
+    void update(const char* data, size_t length);
+
+    uint64_t value() const;
+
+private:
+    uint64_t m_value;
+};
+
+}
+}
+
+#include "linkoptions.h"
+
+#endif
